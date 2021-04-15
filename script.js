3ac318cee6e38324d1e7c5e63e57723ad74a3510
Alexey <48391341+alexeylavr@users.noreply.github.com>
2021-04-15 11:22:49 +0300

    multiplayer

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-let type = ['block', 'flex'];
-
-
 const page1 = document.getElementById('main_page');
 const page2 = document.getElementById('settings');
-const page3 = document.getElementById('choose-dificulty');
+const page3 = document.getElementById('online-option');
+const page4 = document.getElementById('choose-dificulty');
+
+let openedPage = page1;
 
 const settingsButton = document.getElementsByClassName('header__text__img');
-const back = document.getElementById('back');
+const multiButton = document.getElementById('multi');
+const backSettings = document.getElementById('back-settings');
+const backMultiplayer = document.getElementById('back-multiplayer');
 
-settingsButton[0].addEventListener('click',() => openNextPage(page1,page2));
-back.addEventListener('click',() => openNextPage(page2,page1));
+settingsButton[0].addEventListener('click',() => openNextPage(openedPage,page2));
+multiButton.addEventListener('click',()=>openNextPage(page1,page3));
+backSettings.addEventListener('click',() => openNextPage(openedPage,page1));
+backMultiplayer.addEventListener('click',() => openNextPage(openedPage,page1));
 
 function openNextPage(prev, next){
     prev.style.display = 'none';
     next.style.display = 'flex';
+	openedPage = next;
 }
