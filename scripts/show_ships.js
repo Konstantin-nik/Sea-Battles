a002ea5a49c0876941c435d90395055c46ebd8c9
Konstantin-nik <konstantin.nikolaenka@gmail.com>
2021-05-23 22:22:47 +0300

    img + move mechanics

--- scripts/show_ships.js
+++ scripts/show_ships.js
@@ -6,3 +6,58 @@ loadShips.addEventListener('click', () => showShips(ships));
 function showShips(ship) {
 	ship.style.display = 'block';
 }
+
+
+
+var dragObject;
+var move = document.getElementById('to_move');
+var shiftX;
+var shiftY;
+
+document.onmousedown = function(e) {
+    for(n of move.children) {
+        if(n.id == e.target.id) {
+            dragObject = e.target;
+            shiftX = e.pageX - getCoords(dragObject).left;
+	        shiftY = e.pageY - getCoords(dragObject).top;
+
+            document.onmousemove = function(e) {
+                moveAt(e);
+            };
+
+            dragObject.onmouseup = function(e) {
+                dragObject = null;
+            };
+
+                function moveAt(e) {
+                dragObject.style.position = 'absolute';
+                document.getElementById("debug").innerHTML = shiftX + ':' + shiftY;
+                document.getElementById("debug2").innerHTML = dragObject.id;
+                dragObject.style.left = e.pageX - shiftX + 'px';
+                dragObject.style.top = e.pageY - shiftY + 'px';
+                return;
+            };
+        }
+    }
+}
+
+function myFunc() {
+    var x = document.getElementsByClassName("hhh");
+      x[1].style.display = "none";
+      x[0].style.display = "none";
+    
+}
+
+function getCoords(elem) {
+	let box = elem.getBoundingClientRect();
+  
+	return {
+	  top: box.top,
+	  left: box.left
+	};
+}
+
+move.ondragstart = function() {
+	return false;
+}
+
