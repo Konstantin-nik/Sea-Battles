84cc5d395882bd279b131a82791f30457cae8c1e
Konstantin-nik <konstantin.nikolaenka@gmail.com>
2021-05-24 10:00:22 +0300

    modified

--- scripts/show_ships.js
+++ scripts/show_ships.js
@@ -1,5 +1,8 @@
 const loadShips = document.getElementById('loadShips');
 const ships = document.getElementById('ships');
+window.onresize = function() {
+	toPosition();
+}
 
 loadShips.addEventListener('click', () => showShips(to_move));
 
@@ -25,6 +28,7 @@ move.onmousedown = function(e) {
 
             dragObject.onmouseup = function(e) {
 				dragObject.style.zIndex = 900;
+				//dragObject.style.position = 'relative';
                 dragObject = null;
             };
 
@@ -44,11 +48,33 @@ function getCoords(elem) {
   
 	return {
 	  top: box.top,
-	  left: box.left
+	  left: box.left,
+	  bottom: box.top + box.height,
+	  right: box.left + box.width
 	};
 }
 
-move.ondragstart = function() {
+var n;
+var sss;
+function toPosition() {
+	sss = 10;
+	move.children[0].style.left = getCoords(loadShips).left + 'px';
+	move.children[0].style.top = getCoords(loadShips).bottom + 20 + 'px' ;
+	var l = move.children.length;
+	for(let i = 1; i < l - 2; i++) {
+		n = move.children[i];
+		n.style.left = getCoords(move.children[i -1]).left + 'px';
+		n.style.top = getCoords(move.children[i -1]).bottom + 3 + 'px';
+	}
+	move.children[l-2].style.left = getCoords(move.children[l-4]).right + 4 + 'px';
+	move.children[l-2].style.top = getCoords(move.children[l-4]).top + 'px';
+	move.children[l-1].style.left = getCoords(move.children[l-3]).right + 4 + 'px';
+	move.children[l-1].style.top = getCoords(move.children[l-3]).top + 'px';
+
+}
+
+document.ondragstart = function() {
 	return false;
 }
 
+toPosition();
